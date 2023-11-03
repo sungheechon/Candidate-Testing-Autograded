f31b8c0c60e0e875d40ce09372bd864dfb394de1
sunghee chon <sungheechon@sunghees-imac.lan>
2023-11-03 15:41:20 -0400

    Graded Assignment 1: Candidate Testing, Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,9 +12,10 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = ["", "", "", "", ""];
+
 
 
 function askForName() {
@@ -25,20 +26,51 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  question = input.question(question);
-  candidateAnswer = question;
+  for (let i = 0; i < questions.length; i++) {
+    questions[i] = input.question(questions[i]);
+    candidateAnswers[i] = questions[i];
+  }
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer.toLowerCase().trim() == correctAnswer.toLowerCase()) {
-    console.log("Correct Answer!")
-  } else {
-    console.log("Sorry, that is incorrect.")
-  }
 
+
+  console.log(`
+Candidate Name: ${candidateName}
+1) Who was the first American woman in space?
+Your Answer: ${candidateAnswers[0]}
+Correct Answer: ${correctAnswers[0]}
+
+2) True or false: 5000 meters = 5 kilometers.
+Your Answer: ${candidateAnswers[1]}
+Correct Answer: ${correctAnswers[1]}
+
+3) (5 + 3)/2 * 10 = ?
+Your Answer: ${candidateAnswers[2]}
+Correct Answer: ${correctAnswers[2]}
+
+4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
+Your Answer: ${candidateAnswers[3]}
+Correct Answer: ${correctAnswers[3]}
+
+5) What is the minimum crew size for the ISS?
+Your Answer: ${candidateAnswers[4]}
+Correct Answer: ${correctAnswers[4]}
+`)
+
+
+  /* 
+  SUNGHEE START: Part 1 Code
+    // if (candidateAnswers.toLowerCase().trim() == correctAnswers.toLowerCase()) {
+    //   console.log(`${candidateAnswers} is correct!`);
+    // } else {
+    //   console.log(`Sorry, ${candidateAnswers} is incorrect.`);
+    // }
+  SUNGHEE END:   Part 1 Code
+  */
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
